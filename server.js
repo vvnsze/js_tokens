269322448831780fb3097c0cec6a0c94762d9865
vvnsze <vvnsze@gmail.com>
2016-11-04 22:26:29 -0700

    set up back end

--- server.js
+++ server.js
@@ -6,4 +6,20 @@ var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 var config = require('./config');
 var User = require('./app/models/user');
-var port = process.env.port 
+var port = process.env.port || 8080;
+
+mongoose.connect(config.database);
+
+app.set('secret', config.secret);
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use(morgan('dev'));
+
+app.get('/',function(req,res){
+  res.send('this api is from localhost' + port + '/api')
+});
+
+app.listen(port);
+console.log("how does this log? http://localhost:" + port);

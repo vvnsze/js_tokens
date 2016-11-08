e805976c56672cfa903a05d263e04d33d8dfacfd
vvnsze <vvnsze@gmail.com>
2016-11-07 17:44:27 -0800

    working on tokens

--- server.js
+++ server.js
@@ -23,3 +23,64 @@ app.get('/',function(req,res){
 
 app.listen(port);
 console.log("how does this log? http://localhost:" + port);
+
+
+app.get('/setup',function(req,res){
+
+  var jenny = new User({
+    name: 'Jenny Tomato',
+    password: 'password',
+    admin: true
+  });
+
+  jenny.save(function(err){
+    if(err) throw err;
+    console.log('User saved successfully');
+    res.json({success:true});
+  })
+
+})
+
+var apiRoutes = express.Router();
+
+//todo: route to authenticate a user
+
+apiRoutes.post('/authenticate', function(req,res){
+  User.findOne({
+    name: req.body.name
+  }),function(err, user){
+    if (err) throw err;
+
+    if (!user){
+      res.json({success: false, message:'Authentication failed.User Not found.'});
+    } else if (user){
+
+      //check for password matching
+      if(user.password != req.body.password){
+        res.json({success: false, message:'Authentication failed. Wrong password.'});
+      } else {
+
+        var token = jwt.sign(user, app.get('secret'),{
+          expiresinMinutes: 1440
+        });
+
+      }
+
+
+
+
+    }
+
+
+  }
+});
+
+//todo route middleware to verify a token
+
+apiRoutes.get('/users', function(req, res){
+  User.find({}, function(err,users){
+    res.json(users);
+  });
+});
+
+app.use('/api', apiRoutes);
